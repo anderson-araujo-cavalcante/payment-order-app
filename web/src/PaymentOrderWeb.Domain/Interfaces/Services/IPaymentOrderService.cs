b15c6de994fcdddfbdab631b8ddec9e6744f0369
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-18 19:41:13 -0300

    Create IPaymentOrderService.cs

---
+++ web/src/PaymentOrderWeb.Domain/Interfaces/Services/IPaymentOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentOrderWeb.Domain.Interfaces
+{
+    public interface IPaymentOrderService
+    {
+    }
+}
