904bb3aebc66eb429e3761a0bac7505f6a7ad856
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-21 11:45:10 -0300

    removed unnecessary codes

--- web/src/PaymentOrderWeb.Domain/Entities/EmployeeData.cs
+++ web/src/PaymentOrderWeb.Domain/Entities/EmployeeData.cs
@@ -1,13 +1,10 @@
-﻿using CsvHelper.Configuration.Attributes;
-using System.Runtime.CompilerServices;
-
-namespace PaymentOrderWeb.Domain.Entities
+﻿namespace PaymentOrderWeb.Domain.Entities
 {
     public struct EmployeeData
     {
         public int Code { get; set; }
         public string Name { get; set; }
-        public double HourlyRate { get; set; }        
+        public double HourlyRate { get; set; }
         public string LunchTime { get; set; }
         public double TotalReceivable { get; set; }
         public double ExtraHours { get; set; }
@@ -15,11 +12,8 @@ namespace PaymentOrderWeb.Domain.Entities
         public int MissingDays { get; set; }
         public int ExtraDays { get; set; }
         public int WorkedDays { get; set; }
-       // [Format("dd/MM/yyyy")]
         public DateOnly Date { get; init; }
-        //[Format("HH:mm")]
         public TimeOnly EntryTime { get; init; }
-       // [Format("HH:mm")]
         public TimeOnly OutputTime { get; set; }
     }
 }
